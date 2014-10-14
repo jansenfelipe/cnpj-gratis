a923a6072cf89bfe392df1ce7a2ea70cd3d27c1e
Jansen Felipe <jansen.felipe@gmail.com>
2014-10-14 14:17:38 -0300

    Remoção arquivos de teste

--- teste2.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-
-
-$cnpjGratis = new \JansenFelipe\CnpjGratis\CnpjGratis();
-
-$empresa = $cnpjGratis->consulta($_POST['cnpj'], $_POST['captcha'], $_POST['viewstate'], $_POST['cookie']);
-var_dump($empresa);
