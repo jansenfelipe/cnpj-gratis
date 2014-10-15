0f9e99300af2d67230fb8bed1db715a3b31926d8
Jansen Felipe <jansen.felipe@gmail.com>
2014-10-15 00:31:05 -0300

    phpunit

---
+++ tests/JansenFelipe/CnpjGratis/CnpjGratisTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace JansenFelipe\CnpjGratis;
+
+use PHPUnit_Framework_TestCase;
+
+class CnpjGratisTest extends PHPUnit_Framework_TestCase {
+
+    private $params;
+
+    public function testGetParams() {
+
+        $this->params = CnpjGratis::getParams();
+
+        $this->assertEquals(true, isset($this->params['captcha']));
+        $this->assertEquals(true, isset($this->params['viewstate']));
+        $this->assertEquals(true, isset($this->params['cookie']));
+    }
+
+}
